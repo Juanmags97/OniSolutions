ef6a6d420977cf04b67d69d32e4a031a07d8bafa
Juan Manuel Gonzalez <juanmagonz30@gmail.com>
2026-08-29 15:43:18 -0300

    juanma

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/OniSolutions/', // <-- Reemplazá 'nombre-de-tu-repositorio' por el nombre tal cual está en GitHub (ej: /OniSolutions/)
+  base: '/', // <-- Reemplazá 'nombre-de-tu-repositorio' por el nombre tal cual está en GitHub (ej: /OniSolutions/)
 })
